6ba56b28ec85b3c9c488a1d457489a0569015b2d
Student <student@d17-428-pc-20.ipa.ii.agh.edu.pl>
2024-05-18 14:10:22 +0200

    Reaktywnosc

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-    let email = 'fracz@agh.edu.pl';
+    const [email, setEmail] = useState('fracz@agh.edu.pl');
+
 
     function handleChange(event) {
-        console.log(event.target.value);
+        setEmail(event.target.value);
     }
+
+    let message = <div> Masz krótki adress. </div>
+    if (email.length > 15) {
+        message = <div> Masz bardzi długi adress. </div>
+    } else if (email.length > 5) {
+        message = <div> Masz ok adress. </div>;
+    }
+
 
     return (
         <div>
             <h1>System do zapisów na zajęcia</h1>
-            <h2>Twój e-mail to {email}.</h2>
-            <input type="text" onChange={handleChange}/>
+            <h2>Twój e-mail to {email}</h2>
+            {message}
+            <input type="text" value={email} onChange={handleChange}/>
         </div>
     );
 }
